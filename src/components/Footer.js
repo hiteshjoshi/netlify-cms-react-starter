b3bc67cd60d9a3a5c5e018e59433d1eeb194377a
Hitesh Joshi <me@hiteshjoshi.com>
2019-06-03 19:29:08 +0530

    footer date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,11 @@ export default ({
   globalSettings,
   socialSettings,
   navLinks
-}) => ( <
-  footer className = 'Footer' >
-  <
-  div className = 'container taCenter' >
-  <
-  span > ©{
-    new Date().getFullYear()
-  }
-  All rights reserved. < /span> <
-  /div> <
-  /footer>
+}) => ( <footer className = 'Footer' >
+    <div className = 'container taCenter' >
+      <span> ©{ new Date().getFullYear()}
+        All rights reserved.
+      </span>
+  </div>
+</footer>
 )
