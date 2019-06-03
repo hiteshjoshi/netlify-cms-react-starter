6513ceafb9c44195e287330403bf82ae3b0c3719
Hitesh Joshi <me@hiteshjoshi.com>
2019-06-03 19:28:07 +0530

    footer date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 import './Footer.css'
 
-export default ({ globalSettings, socialSettings, navLinks }) => (
-  <footer className='Footer'>
-    <div className='container taCenter'>
-      <span>© 2017 All rights reserved.</span>
-    </div>
-  </footer>
+export default ({
+  globalSettings,
+  socialSettings,
+  navLinks
+}) => ( <
+  footer className = 'Footer' >
+  <
+  div className = 'container taCenter' >
+  <
+  span > ©{
+    new Date().getFullYear()
+  }
+  All rights reserved. < /span> <
+  /div> <
+  /footer>
 )
